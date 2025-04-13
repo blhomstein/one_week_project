33a8bf1f3e1529c5679280fde11391e2f0137843
Assil Jaby <altf4irl@gmail.com>
2025-04-13 20:59:23 +0100

    fix: add secret to docker compose envs

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken";
-import crypto, { sign } from "crypto";
-import dotenv from "dotenv";
+import jwt from 'jsonwebtoken';
+import crypto, { sign } from 'crypto';
+import dotenv from 'dotenv';
 
-dotenv.config({ path: "./.env" });
+// dotenv.config({ path: "./.env" });
 
 function generateAccessToken(user) {
   const signature = jwt.sign(
     { userId: user.id },
     process.env.JWT_ACCESS_TOKEN,
     {
-      expiresIn: "90min",
+      expiresIn: '90min',
     }
   );
 
@@ -17,7 +17,7 @@ function generateAccessToken(user) {
 }
 
 function generateRefereshToken() {
-  const token = crypto.randomBytes(16).toString("base64url");
+  const token = crypto.randomBytes(16).toString('base64url');
   return token;
 }
 
